652f45019d394c7af3c854e81072fccf73ed526a
NetAcad DEVASC <changeme@example.com>
2023-11-29 11:18:01 +0000

    q22

---
+++ midterm_app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
